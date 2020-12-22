39f57fc534b3bc62af2bd8d57d6667a61dec46ab
emirdeliz <emirdeliz@gmail.com>
2020-12-22 18:42:34 -0300

    feat: added the border-radius theme

---
+++ src/system/theme/border-radius.ts
@@ -0,0 +1,14 @@
+/**
+ * =========== (c) 2020 Emir Marques ===========
+ * @created on Mon Dec 21 2020
+ * @author Emir Marques - <emirdeliz@gmail.com>
+ * What is this file?
+ * This file is responsible for defining the app border-radius.
+ * ================================================
+ */
+export enum EBorderRadius {
+  small = '1px',
+  normal = '3px',
+  large = '5px',
+  xLarge = '10px'
+}

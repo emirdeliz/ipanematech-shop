0ca9def7faf3aa2ce3309fd7a389eafcbf08cfd1
emirdeliz <emirdeliz@gmail.com>
2020-12-22 18:40:30 -0300

    feat: renamed the hooks to keep the pattern

---
+++ src/hooks/use-translate.ts
@@ -0,0 +1,16 @@
+/**
+ * =========== (c) 2020 Emir Marques ===========
+ * @created on Mon Dec 21 2020
+ * @author Emir Marques - <emirdeliz@gmail.com>
+ * What is this file?
+ * This file is responsible for define the translate object
+ * =============================================
+ */
+import { useI18n } from 'next-rosetta';
+import { ILocale } from '@i18n/template';
+
+export const useTranslate = () => {
+  const i18n = useI18n<ILocale>();
+  const { t } = i18n;
+  return t;
+};

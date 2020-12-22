1d8f9c415aa66f1458f8d64ba7fcd96c9b0076d4
emirdeliz <emirdeliz@gmail.com>
2020-12-22 18:40:58 -0300

    feat: added the shop-product hook

---
+++ src/hooks/use-shop-product.ts
@@ -0,0 +1,43 @@
+/**
+ * =========== (c) 2020 Emir Marques ===========
+ * @created on Mon Dec 21 2020
+ * @author Emir Marques - <emirdeliz@gmail.com>
+ * What is this file?
+ * This hook is responsible for define the shop-product
+ * =============================================
+ */
+import { useEffect, useState } from 'react';
+import { ShopProductsRepository } from '@api/repository/shop-product-repository';
+import { ShopProductModel } from '@api/model/shop-product-model';
+
+export const useShopProduct = () => {
+  const [products, setProducts] = useState<Array<ShopProductModel>>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  /**
+   * @created on Mon Dec 22 2020
+   * @author Emir Marques - <emirdeliz@gmail.com> 
+   * This method is used to return the store related products that are in the repository
+   */
+  const findProducts = async () => {
+    setLoading(true);
+    const result = ShopProductsRepository.findProducts();
+    setLoading(false);
+    return result;
+  }
+
+  /**
+   * @created on Mon Dec 22 2020
+   * @author Emir Marques - <emirdeliz@gmail.com> 
+   * This method is used to initialize the hook
+   */
+  const initialize = async() => {
+    const products = await findProducts();
+    setProducts(products);
+  }
+
+  useEffect(() => {
+    initialize();
+  }, [])
+  return { findProducts, products, loading };
+};

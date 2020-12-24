1a7aafa0f43d927a22ea923cd97b0ffa3a781834
emirdeliz <emirdeliz@gmail.com>
2020-12-24 07:18:28 -0300

    feat: added the theme docs

--- src/hooks/use-theme.ts
+++ src/hooks/use-theme.ts
@@ -17,23 +17,55 @@ interface IUseTheme {
 
 export const useTheme = (): IUseTheme => {
   const [themeType, setThemeType] = useState<EThemeType>(EThemeType.light);
-  const setMode = (mode: EThemeType) => {
+
+  /**
+   * @created on Mon Dec 23 2020
+   * @author Emir Marques - <emirdeliz@gmail.com>
+   * @description This method is used to update the theme in the state
+   * @param mode: selected theme
+   */
+  const _setMode = (mode: EThemeType) => {
     window.localStorage.setItem('themeType', mode);
     setThemeType(mode);
   };
 
-  const isCurrentLight = () => themeType === EThemeType.light;
+  /**
+   * @created on Mon Dec 23 2020
+   * @author Emir Marques - <emirdeliz@gmail.com>
+   * @description This method is used to check if the current theme is light 
+   */
+  const _isCurrentLight = () => themeType === EThemeType.light;
+
+  /**
+   * @created on Mon Dec 23 2020
+   * @author Emir Marques - <emirdeliz@gmail.com>
+   * @description This method is used to update the theme 
+   */
   const themeToggler = () => {
-    isCurrentLight() ? setMode(EThemeType.dark) : setMode(EThemeType.light);
+    _isCurrentLight() ? _setMode(EThemeType.dark) : _setMode(EThemeType.light);
   };
 
+  /**
+   * @created on Mon Dec 23 2020
+   * @author Emir Marques - <emirdeliz@gmail.com>
+   * @description This method is used to get the selected theme 
+   */
   const getTheme = useCallback(() => {
-    return isCurrentLight() ? LightTheme : DarkTheme;
+    return _isCurrentLight() ? LightTheme : DarkTheme;
   }, [themeType]);
 
-  useEffect(() => {
+  /**
+   * @created on Mon Dec 23 2020
+   * @author Emir Marques - <emirdeliz@gmail.com>
+   * @description TThis method is used to initialize the hook 
+   */
+  const _initialize = () => {
     const localTheme = window.localStorage.getItem('themeType');
     localTheme && setThemeType(localTheme as EThemeType);
+  }
+
+  useEffect(() => {
+    _initialize();
   }, []);
 
   return { themeType, themeToggler, theme: getTheme() };
